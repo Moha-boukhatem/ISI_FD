ec9769f415ae58304c58f44a52932158eb835ee6
moha-boukhatem <moha94orly@gmail.com>
2021-04-26 13:32:01 +0100

    update data

--- TP1/TP1.R
+++ TP1/TP1.R
@@ -1,1 +1,16 @@
-data = read.csv('data.csv')
+data = read.csv('../data.csv')
+data$Age = ifelse(is.na(data$Age),
+            ave(data$Age,FUN = function(x) mean(x, na.rm=TRUE)),
+            data$Age)
+
+data$Salaire = ifelse(is.na(data$Salaire),
+            ave(data$Salaire ,FUN = function(x) mean(x, na.rm=TRUE)),
+            data$Salaire)
+
+
+data$Pays = factor(data$Pays,
+            levels = c('France','Espagne','Allemagne'),
+            labels = c(1,2,3))
+        
+
+print(data)
